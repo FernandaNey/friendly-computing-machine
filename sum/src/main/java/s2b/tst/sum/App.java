fc8f5ee4f20d63cd0235cec05213db5253fe3601
teste <teste@10.32.175.53>
2017-09-12 14:58:38 -0300

    Teste 1

--- sum/src/main/java/s2b/tst/sum/App.java
+++ sum/src/main/java/s2b/tst/sum/App.java
@@ -1,19 +1,21 @@
 package s2b.tst.sum;
 
-/**
- * Hello world!
- *
- */
+import java.util.Scanner;
+
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-    	double a = 10;
-    	double b = 20;
-    	double r = Sum.sum(a, b);
-    	System.out.printf("A= %f\n",a);
-    	System.out.printf("B= %f\n",b);
-    	System.out.printf("Resposta= %f\n", r);
+    public static void main( String[] args ) {
+    Scanner entrada = new Scanner (System.in);
+    System.out.printf("Infome o valor de A:\n");
+    double a = entrada.nextDouble();
+    System.out.printf("Infome o valor de B:\n");
+    double b = entrada.nextDouble();
+    entrada.close();
+    double r = Sum.sum(a, b);
+    System.out.printf("A= %f\n",a);
+	System.out.printf("B= %f\n",b);
+	System.out.printf("Resposta= %f\n", r);
     	
       
     } 
