b3061ace26116c355575a8201aba360978edaf81
FernandaNey <fernanda.ney1989@gmail.com>
2017-09-12 15:02:48 -0300

    Alteração

--- sum/src/main/java/s2b/tst/sum/App.java
+++ sum/src/main/java/s2b/tst/sum/App.java
@@ -7,9 +7,9 @@ public class App
 {
     public static void main( String[] args ) {
     Scanner entrada = new Scanner (System.in);
-    System.out.printf("Infome o valor de A:\n");
+    System.out.printf("Infome o valor A:\n");
     double a = entrada.nextDouble();
-    System.out.printf("Infome o valor de B:\n");
+    System.out.printf("Infome o valor B:\n");
     double b = entrada.nextDouble();
     entrada.close();
     double r = Sum.sum(a, b);
